43511eba621166bbbdf7e814e356af65bb19d195
AMBLCO <matis.berubelauziere@gmail.com>
2021-10-08 18:57:28 -0400

    QT_NMF_APP ui

--- QT_NMF_APP/NMF/NMF/x64/Release/uic/ui_NMF.h
+++ QT_NMF_APP/NMF/NMF/x64/Release/uic/ui_NMF.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e35fc941126be1fd24634ba4143adeac50908a1f6dc5d1b5856adacdaa3f8227
-size 2039
+oid sha256:74b5fb2d55f3cab4ff813d0b957e650a08efaace15cb4e8b361e1933cc2359e7
+size 11070
